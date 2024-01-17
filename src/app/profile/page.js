629650ad9592b35e38b7675feadaef7e52be5115
Ladalo-Daniel <balaladalo@gmail.com>
2024-01-17 19:12:55 +0100

    initial commit

---
+++ src/app/profile/page.js
@@ -0,0 +1,14 @@
+import MainLayout from '@/components/MainLayout'
+import ProfileCard from '@/components/ProfileUI/ProfileCard'
+
+export default function Profile() {
+  return (
+    <>
+      <MainLayout>
+         <>
+          <ProfileCard />
+         </>
+      </MainLayout>
+    </>
+  )
+}
